109c53edbf4b0f2b1f6a2824bd04fd9fafcbc522
tjl <2740942511@qq.com>
2019-12-31 15:48:22 +0800

    选择排序

--- src/tjl/listen6/closure/main.go
+++ src/tjl/listen6/closure/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func Add() func(int) int {
 	var x int
@@ -19,6 +23,49 @@ func testClosure1() {
 	fmt.Printf("ret =%d", ret)
 
 }
+func makeSuffixFunc(suffix string) func(string) string {
+	return func(name string) string {
+		if !strings.HasSuffix(name, suffix) {
+			return name + suffix
+
+		}
+		return name
+	}
+}
+func testClosure3() {
+	func1 := makeSuffixFunc(".bmp")
+	func2 := makeSuffixFunc(".jpg")
+	fmt.Println(func1("test"))
+	fmt.Println(func2("test"))
+}
+func calc(base int) (func(int) int, func(int) int) {
+	add := func(i int) int {
+		base += i
+		return base
+	}
+	sub := func(i int) int {
+		base -= i
+		return base
+	}
+	return add, sub
+}
+func testClosure4() {
+	f1, f2 := calc(10)
+	fmt.Println(f1(1), f2(2))
+	fmt.Println(f1(3), f2(4))
+	fmt.Println(f1(5), f2(6))
+}
+func testClosure5() {
+	for i := 0; i < 5; i++ {
+		go func(index int) {
+			fmt.Println(index)
+		}(i)
+	}
+	time.Sleep(time.Second)
+}
 func main() {
-	testClosure1()
+	//testClosure1()
+	//testClosure3()
+	//testClosure4()
+	testClosure5()
 }
---
+++ src/tjl/listen6/sort/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+//插入排序
+func insert_sort(a [8]int) [8]int {
+	for i := 1; i < len(a); i++ {
+		for j := i; j > 0; j-- {
+			if a[j] < a[j-1] {
+				a[j], a[j-1] = a[j-1], a[j]
+			} else {
+				break
+			}
+		}
+	}
+
+	return a
+}
+
+//选择排序
+func select_sort(a [8]int) [8]int {
+	for i := 0; i < len(a); i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[j] < a[i] {
+				a[i], a[j] = a[j], a[i]
+			}
+		}
+	}
+	return a
+}
+func main() {
+	var i [8]int = [8]int{1, 2, 4, 5, 7, 8, 3, 6}
+	j := insert_sort(i)
+	k := select_sort(i)
+	fmt.Println(k)
+	fmt.Println(i)
+	fmt.Println(j)
+}
